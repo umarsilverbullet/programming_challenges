a59e0b8b81090678f792d95509c41f0f8370db3e
umarsilverbullet <umarkhan.shakeel@gmail.com>
2022-09-23 18:05:51 +0100

    reached 848,976 rank

--- problems/BuyAndSellStock.py
+++ problems/BuyAndSellStock.py
@@ -20,6 +20,11 @@ class BuyAndSellStock:
 
     def solveProblem2(self,prices):
         print(f'prices = {prices}')
+        dp = [[-1 for j in range(2)] for k in  range( len(prices))]
+        maxProfit = self.solveStockII(prices,0,1,dp);
+        return maxProfit
+
+
         maxProfit = 0
         for i in range(len(prices)):
             buy = prices[i]
@@ -28,3 +33,90 @@ class BuyAndSellStock:
                 maxProfit = max(maxProfit,profit)
 
         return maxProfit
+
+    def buyAndSellStockII(self,prices):
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i] >= prices[i-1]:
+                profit += prices[i] - prices[i-1]
+
+        return profit
+
+    def buyAndSellStockIII(self,prices):
+        limit = 2
+        dp = [[ [-1 for k in range(3)] for j in range(2) ]  for i in range(len(prices))]
+        maxProfit = self.solveStockIII(prices, 0, 1, 2, dp)
+        return maxProfit
+
+        #  other non-working method which did not work.
+        totalProfit = 0
+        transactions = 2
+        l = 0
+        r = 1
+        for i in range(transactions):
+            r = l+1
+            profit = 0
+            while r < len(prices):
+
+                if prices[r] < prices[l]:
+                    totalProfit += profit
+                    l = r
+                    break
+                else:
+                    profit = max(profit,prices[r] - prices[l])
+
+                r+=1
+
+            if r >= len(prices):
+                totalProfit += profit
+                break
+
+        return totalProfit
+
+    def solveStockIII(self,prices,index,buy,limit,dp):
+
+        if index == len(prices):
+            return 0
+
+        if limit == 0:
+            return 0
+
+        if dp[index][buy][limit] != -1:
+            return dp[index][buy][limit]
+
+        profit =0
+
+        if (buy):
+            buyProfit = -prices[index] + self.solveStockIII(prices, index+1, 0, limit, dp)
+            skipBuyProfit = 0 + self.solveStockIII(prices, index+1, 1, limit, dp)
+            profit = max(buyProfit,skipBuyProfit)
+        else:
+            sellProfit = prices[index] + self.solveStockIII(prices, index+1, 1, limit-1, dp)
+            skipSellProfit = 0 + self.solveStockIII(prices, index+1, 0, limit, dp)
+            profit = max(sellProfit, skipSellProfit)
+
+        dp[index][buy][limit] = profit
+        return profit
+
+    # Does not work till now
+    def solveStockII(self,prices, index, buy, dp):
+        print(f'dp === {dp}')
+        if index == len(prices):
+            return 0
+
+        if dp[index][buy] != -1:
+            return dp[index][buy]
+
+        profit = 0
+        if buy:
+            buyAtIndex = -prices[index] + self.solveStockII(prices,index+1,0,dp)
+            skipBuyAtIndex = 0 + self.solveStockII(prices,index+1,0,dp)
+            profit = max(buyAtIndex,skipBuyAtIndex)
+        else:
+            sellAtIndex = prices[index] + self.solveStockII(prices, index+1, 1,dp)
+            skipSellAtIndex = 0 + self.solveStockII(prices, index+1, 0,dp)
+            profit = max(sellAtIndex, skipSellAtIndex)
+
+        print(f" dp [{index}][{buy}] = {dp[index][buy]}");
+        dp[index][buy] = profit
+        return profit
--- problems/test_BuyAndSellStock.py
+++ problems/test_BuyAndSellStock.py
@@ -1,18 +1,19 @@
 from unittest import TestCase
 from BuyAndSellStock import BuyAndSellStock
 
+
 class TestBuyAndSellStock(TestCase):
     def setUp(self) -> None:
         self.bss = BuyAndSellStock()
 
     def test_solve_problem(self):
-        prices = [7,1,5,3,6,4]
+        prices = [7, 1, 5, 3, 6, 4]
         expected = 5
 
         prices = [7, 6, 4, 3, 1]
         expected = 0
 
-        prices = [2,1,2,1,0,1,2]
+        prices = [2, 1, 2, 1, 0, 1, 2]
         expected = 2
 
         prices = [1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9]
@@ -20,4 +21,42 @@ class TestBuyAndSellStock(TestCase):
 
         output = self.bss.solveProblem(prices)
         print(f'output={output}')
-        self.assertEqual(expected,output,f'expected = {expected}   =====    output = {output}')
+        self.assertEqual(expected, output, f'expected = {expected}   =====    output = {output}')
+
+    def test_solve_problem2(self):
+        prices = [7, 1, 5, 3, 6, 4]
+        expected = 7
+
+        output = self.bss.solveProblem2(prices)
+        print(f'output={output}')
+        self.assertEqual(expected, output, f'expected = {expected}   =====    output = {output}')
+
+    def test_buy_and_sell_stock_ii(self):
+        prices = [7, 1, 5, 3, 6, 4]
+        expected = 7
+
+        output = self.bss.buyAndSellStockII(prices)
+        print(f'output={output}')
+        self.assertEqual(expected, output, f'expected = {expected}   =====    output = {output}')
+
+    def test_buy_and_sell_stock_iii(self):
+        prices = [3, 3, 5, 0, 0, 3, 1, 4]
+        expected = 6
+
+        prices = [1, 2, 3, 4, 5]
+        expected = 4
+
+        # prices = [7,6,4,3,1]
+        # expected = 0
+
+        prices = [1, 4, 2]
+        expected = 3
+
+        prices = [2, 1, 2, 0, 1]
+        expected = 2
+
+        output = self.bss.buyAndSellStockIII(prices)
+        print(f'output={output}')
+        self.assertEqual(expected, output, f'expected = {expected}   =====    output = {output}')
+
+
